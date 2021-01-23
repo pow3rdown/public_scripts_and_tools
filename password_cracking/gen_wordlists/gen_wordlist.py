2d667acf01cca2c08d119611f2387dfb30e17348
rodrigormartins <63931326+rodrigormartins@users.noreply.github.com>
2021-01-23 15:43:29 +0000

    Add files via upload

--- password_cracking/gen_wordlists/gen_wordlist.py
+++ password_cracking/gen_wordlists/gen_wordlist.py
@@ -1,1 +1,158 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+
+fname = sys.argv[1]
+
+def load_file():
+
+        with open(fname) as f:
+                content = [x.strip() for x in f.readlines()]
+                for line in content:
+                        if line and not re.match('^#.*', line):
+                                word_rules(line)
+
+
+def gen_numeric_seq():
+
+        global numbers
+
+        numbers         = []
+        numbers_temp2   = []
+        numbers_temp1   = map(str,range(9999))
+
+        for n in numbers_temp1:
+                numbers_temp2.append(n.zfill(4))
+
+        numbers         = sorted(set(numbers_temp1 + numbers_temp2))
+        numbers_temp1   = numbers_temp2 = []
+
+
+def word_rules(word):
+        print word
+        print word + "@@"
+        print word + "//"
+        print word + ".."
+        print word + ",,"
+        print word + "@" + "0"
+        print word + "@" + "1"
+        print word + "@" + "12"
+        print word + "@" + "123"
+        print word + "@" + "1234"
+        print word + "@" + "12345"
+        print word + "@" + "123456"
+        print word + "@" + "1234567"
+        print word + "@" + "12345678"
+        print word + "@" + "123456789"
+        print word + "@" + "1234567890"
+        print word + "@" + "0123456789"
+
+        print word + "12345"
+        print word + "123456"
+        print word + "1234567"
+        print word + "12345678"
+        print word + "123456789"
+        print word + "1234567890"
+        print word + "0123456789"
+
+        print "0" + "@" + word
+        print "1" + "@" + word
+        print "12" + "@" + word
+        print "123" + "@" + word
+        print "1234" + "@" + word
+        print "12345" + "@" + word
+        print "123456" + "@" + word
+        print "1234567" + "@" + word
+        print "12345678" + "@" + word
+        print "123456789" + "@" + word
+        print "1234567890" + "@" + word
+        print "0123456789" + "@" + word
+
+        print "12345" + word
+        print "123456" + word
+        print "1234567" + word
+        print "12345678" + word
+        print "123456789" + word
+        print "1234567890" + word
+        print "0123456789" + word
+
+        print word + "@" + "admin"
+        print "admin" + "@" + word
+
+        for num in numbers:
+
+                print "@" + num + word
+                print num + "!" + word
+                print num + "#" + word
+                print num + "*" + word
+                print num + "**" + word
+                print num + "@" + word
+                print num + "@" + word + "!"
+                print num + word + "@"
+                print num + word + "."
+                print num + word + "$$"
+                print num + word + "$$$"
+
+                print "#" + word + "@" + num
+                print "*" + word + num
+                print "#" + word + num
+                print "#" + word + "." + num
+                print "#" + word + num + "$"
+                print "@" + word + num
+                print "@@" + word + num
+                print "!@#" + word + num
+                print word + "!" + num
+                print word + "!" + num + "!!"
+                print word + "!" + num + "@@"
+                print word + "#" + num
+                print word + "#" + num + "@"
+                print word + "##" + num
+                print word + "$" + num
+                print word + "$" + num + "$"
+                print word + "%" + num
+                print word + "*" + num
+                print word + "*" + num + "!"
+                print word + "**" + num
+                print word + "+" + num
+                print word + "," + num
+                print word + "-" + num
+                print word + "." + num
+                print word + "@" + num
+                print word + "@" + num + "!!"
+                print word + "@" + num + "#"
+                print word + "@" + num + "##"
+                print word + "@" + num + ")"
+                print word + "@" + num + "*"
+                print word + "@" + num + "**"
+                print word + "@" + num + "*-"
+                print word + "@" + num + "."
+                print word + "@" + num + "/."
+                print word + "@" + num + "@"
+                print word + "@" + num + "@:"
+                print word + "@" + num + "@;"
+                print word + "@#" + num
+                print word + "@!" + num
+                print word + "@#$" + num
+                print word + "@@" + num
+                print word + "_" + num
+                print word + num
+                print word + num + "!"
+                print word + num + "#"
+                print word + num + "@"
+                print word + num + "*"
+                print word + num + "."
+                print word + num + "**"
+                print word + num + '$$'
+
+
+def main():
+
+        gen_numeric_seq()
+        load_file()
+
+
+if __name__ == '__main__':
+        main()
 
--- password_cracking/gen_wordlists/gen_wordlists_combination.py
+++ password_cracking/gen_wordlists/gen_wordlists_combination.py
@@ -1,1 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+
+items       = []
+item_temp   = []
+
+files = sorted(glob.glob('*.dic'), key=os.path.getsize)
+for f in files:
+
+    # calculate the number of lines of each file
+    fcontent = open(f)
+    lines = 0
+    buf_size = 1024 * 1024
+    read_f = fcontent.read
+
+    buf = read_f(buf_size)
+    while buf:
+        lines += buf.count('\n')
+        buf = read_f(buf_size)
+
+    fcontent.close()
+    fsize = lines
+
+    item_temp.append(f)
+    item_temp.append(fsize)
+    items.append(item_temp)
+    item_temp   = []
+
+
+final_items     = []
+temp_f_items    = []
+
+for i in items:
+    for j in items:
+        temp_t_size = i[1] * j[1]
+        temp_f_items.append(temp_t_size)
+        temp_f_items.append(i[0])
+        temp_f_items.append(j[0])
+
+        final_items.append(temp_f_items)
+
+        temp_f_items = []
+
+last_t_size = 0
+for f_i in sorted(final_items):
+    if last_t_size <> f_i[0]:
+        print '\n'
+    print 'hashcat --hwmon-temp-abort=100 -O --force --opencl-device-types 1,2 -w 3 -a 1 -m 1000 SAMFILE.SAM ' + str(f_i[1]) + ' ' + str(f_i[2]) + '\t\t# ' + str('{:1,}'.format(f_i[0]))
+    last_t_size = f_i[0]
 
